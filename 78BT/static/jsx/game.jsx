ee0673d67fc68964def59b97cffedbfb578b2ae8
Yuxin Zhou <yqkqknct@gmail.com>
2016-09-07 23:18:33 +0800

    Polishing the UI.

--- 78BT/static/jsx/game.jsx
+++ 78BT/static/jsx/game.jsx
@@ -3,9 +3,9 @@
     var Table = React.createClass({
         render: function() {
             var cards = this.props.cards.map(function(v){
-                return (<span>{v}</span>);
+                return (<span className={'card'}>{v}</span>);
             });
-            return (<div>current_card: {cards}</div>);
+            return (<div className={'current_card'}><span className={'current_card_title'}>Current Card</span><div className={'current_card_board'}>{cards}</div></div>);
         }
     });
     var Player = React.createClass({
@@ -29,7 +29,7 @@
             var that = this;
             var ch_c = this.props.cards.map(function(v, i){
                 if(that.state.status[i]){
-                    return (<span>{v}</span>);
+                    return (<span className={'card'}>{v}</span>);
                 }
                 return (<span/>);
             });
@@ -44,25 +44,27 @@
             var that = this;
             var start_btn = null;
             if(this.props.start_btn){
-                start_btn = (<button
+                start_btn = (<button className={'btn btn-success btn-start'}
                     onClick={function(){that.props.onClick({'type':'start'})}}>開始</button>);
             }
             var reset_btn = null;
             if(this.props.reset_btn){
-                reset_btn = (<button
+                reset_btn = (<button className={'btn btn-danger btn-reset'}
                     onClick={function(){that.props.onClick({'type':'reset'})}}>重設</button>);
             }
             return (
                 <div>
-                    <h4>{this.props.name}</h4>
-                    <div>{ch_c}</div>
-                    <div>{card}</div>
+                    <h4 className={'name'}>{this.props.name}</h4>
+                    <div className={'user_card'}>{ch_c}</div>
+                    <div className={'user_card_set'}>{card}</div>
                     <div>
-                        {start_btn}
-                        {reset_btn}
-                        <button disabled={!this.props.is_your_turn} onClick={function(){that.handleTrow('change')}}>換牌</button>
-                        <button disabled={!this.props.is_your_turn} onClick={function(){that.handleTrow('throw')}}>出牌</button>
-                        <button disabled={!this.props.is_your_turn} onClick={function(){that.props.onClick({'type':'pick'})}}>抽牌</button>
+                      <div className={'form-inline'}>
+                        <div className={'form-group'}>{start_btn}</div>
+                        <div className={'form-group'}>{reset_btn}</div>
+                        <div className={'form-group'}><button className={'btn btn-info btn-change'} disabled={!this.props.is_your_turn} onClick={function(){that.handleTrow('change')}}>換牌</button></div>
+                        <div className={'form-group'}><button className={'btn btn-info btn-throw'} disabled={!this.props.is_your_turn} onClick={function(){that.handleTrow('throw')}}>出牌</button></div>
+                        <div className={'form-group'}><button className={'btn btn-info btn-pick'} disabled={!this.props.is_your_turn} onClick={function(){that.props.onClick({'type':'pick'})}}>抽牌</button></div>
+                      </div>
                     </div>
                 </div>
             );
@@ -120,15 +122,17 @@
         render: function() {
             var that = this;
             var players = this.state.players.map(function(v, i){
-                var turn = (<span>Not Turn</span>);
-                if(that.state.turn_num == i)turn = <span>Turn!!</span>
-                return (<div>Name: {v['name']}, card: {v['card']}, conn: {Number(v['conn'])}, {turn}</div>);
+                var turn = (<span className={'not-turn'}>Not Turn</span>);
+                if(that.state.turn_num == i)turn = <span className={'turn'}>Turn!</span>
+                return (<div className={'player'}><span className={'player-name'}>{v['name']}</span><span className={'player-card'}><span className={'card_icon'}></span><span className={'card_num'}>{v['card']}</span></span><span className={Number(v['conn'])==1?'avail player-conn':'unavail player-conn'}>{Number(v['conn'])==1?'Online':'Offline'}</span>{turn}</div>);
             });
-            return (<div>
-                <div>Online: {this.state.online_user}</div>
-                <div>Status: {this.state.status}</div>
+            return (<div className={'thegame'}>
+
+                <div className={'status'}><span className={'attr_title'}>Status</span>{this.state.status}</div>
                 <Table cards={this.state.current_card}/>
+                <div className={'gametable'}>
                 {players}
+                </div>
                 <Player name={this.state.your_name}
                     cards={this.state.your_card}
                     start_btn={this.state.your_name==this.state.room_manager && this.state.status=='init'}
